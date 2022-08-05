5ec345894e63e4c76ec0e39b0fe9899ec79518ce
Matthew Aitchison <u6857890@focal-master.csitlabs>
2022-08-05 13:54:49 +1000

    added documentation

--- src/DrawElementFactory.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Color;
-import java.awt.Point;
-
-public interface DrawElementFactory {
-	DrawElement createElementFromMousePress(String toolcommand, Color color, Point pos);
-	DrawElement createElementFromLoadFacade(String name, LoadFacade lf);
-	void addbuttons(ToolBar drawtool);
-}
